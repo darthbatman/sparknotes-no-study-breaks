3cb1ce147156b6f3ef5eaef7eea383be248da33f
darthbatman <darthanakin44@gmail.com>
2016-08-30 20:14:48 -0400

    first commit

---
+++ hide.js
@@ -0,0 +1,5 @@
+var takeBreak = document.getElementsByClassName("white-background border-blue blog-feature")[0];
+takeBreak.setAttribute("hidden", "true");
+
+var bnBlog = document.getElementsByClassName("light-grey-white-background border-blue padding-15 margin-btm-30 mobile-hide")[0];
+bnBlog.setAttribute("hidden", "true");
