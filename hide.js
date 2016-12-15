683495e3905aaaabe04871932b46798701870466
darthbatman <darthanakin44@gmail.com>
2016-12-14 19:21:58 -0500

    packaged for chrome web store

--- hide.js
+++ hide.js
@@ -1,5 +1,2 @@
-var takeBreak = document.getElementsByClassName("white-background border-blue blog-feature")[0];
-takeBreak.setAttribute("hidden", "true");
-
-var bnBlog = document.getElementsByClassName("light-grey-white-background border-blue padding-15 margin-btm-30 mobile-hide")[0];
-bnBlog.setAttribute("hidden", "true");
+var sidePanel = document.getElementsByClassName("col-1-3 last")[0];
+sidePanel.setAttribute("hidden", "true");
